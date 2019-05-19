6c858acb77e2fd121be2ac5eb11c64daa07e9878
Unknown <sukhjinderbhullar@gmail.com>
2019-05-19 21:29:00 +1000

    auto scaling policy

---
+++ automation-aws-with-python/02-notifon/autoscale_exmaple.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+import boto3
+session = boto3.Session(profile_name='default')
+as_client = session.client('autoscaling')
+as_client.describe_auto_scaling_groups()
+as_client.describe_policies()
+as_client.execute_policy(AutoScalingGroupName='Notifon Example Group', PolicyName='Scale Up')
+as_client.execute_policy(AutoScalingGroupName='Notifon Example Group', PolicyName='Scale Down')
